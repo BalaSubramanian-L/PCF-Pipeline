5b4c183d7242576bfe02abe805e5d0045b6c1971
BalaSubramanian-L <lbalaeee@gmail.com>
2018-01-24 15:27:05 -0500

    Update task.sh

--- tasks/apply-changes/task.sh
+++ tasks/apply-changes/task.sh
@@ -16,13 +16,11 @@ set -eu
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-echo "Applying changes on Ops Manager @ ${OPSMAN_DOMAIN_OR_IP_ADDRESS}"
+echo "Applying changes on Ops Manager @ ${OPSMAN_URI}"
 
 om-linux \
-  --target "https://${OPSMAN_DOMAIN_OR_IP_ADDRESS}" \
+  --target "https://${OPSMAN_URI}" \
   --skip-ssl-validation \
-  --client-id "${OPSMAN_CLIENT_ID}" \
-  --client-secret "${OPSMAN_CLIENT_SECRET}" \
   --username "${OPSMAN_USERNAME}" \
   --password "${OPSMAN_PASSWORD}" \
   apply-changes \
