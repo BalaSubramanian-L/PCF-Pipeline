2ae257cebfd014249acf35b6b16a47021c1c5f95
BalaSubramanian-L <lbalaeee@gmail.com>
2018-01-24 13:25:51 -0500

    Update task.sh

--- install-pcf/vsphere/tasks/import-opsman/task.sh
+++ install-pcf/vsphere/tasks/import-opsman/task.sh
@@ -6,9 +6,6 @@ file_path=`find ./pivnet-opsman-product/ -name *.ova`
 
 echo $file_path
 
-export GOVC_TLS_CA_CERTS=/tmp/vcenter-ca.pem
-echo "$GOVC_CA_CERT" > $GOVC_TLS_CA_CERTS
-
 govc import.spec $file_path | python -m json.tool > om-import.json
 
 cat > filters <<'EOF'
@@ -33,21 +30,14 @@ jq \
   --arg dns "$OM_DNS_SERVERS" \
   --arg ntpServers "$OM_NTP_SERVERS" \
   --arg adminPassword "$OPS_MGR_SSH_PWD" \
-  --arg customHostname "$OPSMAN_DOMAIN_OR_IP_ADDRESS" \
+  --arg customHostname "$OPS_MGR_HOST" \
   --arg network "$OM_VM_NETWORK" \
   --arg vmName "$OM_VM_NAME" \
-  --arg diskType "$OPSMAN_DISK_TYPE" \
+  --arg diskType "$OM_DISK_TYPE" \
   --argjson powerOn $OM_VM_POWER_STATE \
   --from-file filters \
   om-import.json > options.json
 
 cat options.json
 
-if [ -z $OM_VM_FOLDER ]; then
-  govc import.ova -options=options.json $file_path
-else
-  if [ `govc folder.info $OM_VM_FOLDER 2>&1 | grep $OM_VM_FOLDER | awk '{print $2}'` != $OM_VM_FOLDER ]; then
-    govc folder.create $OM_VM_FOLDER
-  fi
-  govc import.ova -folder=$OM_VM_FOLDER -options=options.json $file_path
-fi
+govc import.ova -options=options.json $file_path
